bb1b55c0115a54bbb21d45eaa246bc5b51f713c6
MontesCodes <MontiesWebDev@Gmail.com>
2024-06-01 11:18:20 -0400

    finished Project 9 formValidation

--- app.js
+++ app.js
@@ -15,4 +15,28 @@ form.addEventListener('submit', event => {
   if (email === '') errors.push('Email field cannot be empty');
   if (password === '') errors.push('Password field cannot be empty');
   if (password !== confirmPassword) errors.push('Passwords do not match');
+
+  if (errors.length > 0) {
+    for (let i = 0; i < errors.length; i++) {
+      Toastify({
+        text: errors[i],
+        duration: 4000,
+        gravity: 'top',
+        position: 'center',
+        style: {
+          background: '#df1c24',
+        },
+      }).showToast();
+    }
+  } else {
+    Toastify({
+      text: 'Success! Form submitted successfully!',
+      duration: 4000,
+      gravity: 'top',
+      position: 'center',
+      style: {
+        background: '#4bab4e',
+      },
+    }).showToast();
+  }
 });
